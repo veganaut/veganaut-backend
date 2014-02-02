4389add0ce551368cc45a4ab698c31ed060b1b62
Jonas Wagner <jonas.wagner@epfl.ch>
2014-02-02 12:04:32 +0100

    Add a person model

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-
 /**
  * Module dependencies.
  */
+
+'use strict';
 
 var express = require('express');
 var routes = require('./routes');
@@ -25,12 +26,12 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
