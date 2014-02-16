f8fa86f414be66d8eee54c97470f82ed170f0dc7
Tobias Leugger <leugger.tobias@web.de>
2014-02-16 22:37:13 +0100

    Describing person API methods (registration)

---
+++ test/e2e/Person.spec.js
@@ -0,0 +1,70 @@
+'use strict';
+/* global it, expect */
+
+var h = require('../helpers');
+
+h.describe('Person API methods', function() {
+    it('can register new user', function() {
+        h.runAsync(function(done) {
+            h.request('POST', h.baseURL + 'person')
+                .send({
+                    email: 'doge@mac.dog',
+                    fullName: 'Doge MacDog',
+                    password: 'wow. such secure. so protect.'
+                })
+                .end(function(res) {
+                    expect(res.statusCode).toBe(201);
+
+                    // Some sanity checks on the returned person
+                    expect(res.body.email).toEqual('doge@mac.dog');
+                    expect(res.body.fullName).toEqual('Doge MacDog');
+
+                    // Make sure password is not returned
+                    expect(typeof res.body.password).toEqual('undefined');
+
+                    done();
+                })
+            ;
+        });
+    });
+
+    it('can register as a full user from partial user (that already entered reference code)', function() {
+        h.runAsync(function(done) {
+            h.request('POST', h.baseURL + 'person')
+                .send({
+                    _id: '000000000000000000000003',
+                    email: 'carol@carol.ca',
+                    fullName: 'Carol Curie',
+                    password: 'oh. yeah.'
+                })
+                .end(function(res) {
+                    expect(res.statusCode).toBe(201);
+
+                    // Some sanity checks on the returned person
+                    expect(res.body._id).toEqual('000000000000000000000003');
+                    expect(res.body.email).toEqual('carol@carol.ca');
+                    expect(res.body.fullName).toEqual('Carol Curie');
+
+                    done();
+                })
+            ;
+        });
+    });
+
+    it('cannot re-register already register person', function() {
+        h.runAsync(function(done) {
+            h.request('POST', h.baseURL + 'person')
+                .send({
+                    _id: '000000000000000000000001',
+                    email: 'a@b.ch',
+                    fullName: 'Hacker DeHack',
+                    password: 'ups'
+                })
+                .end(function(res) {
+                    expect(res.statusCode).toBe(403);
+                    done();
+                })
+            ;
+        });
+    });
+});
