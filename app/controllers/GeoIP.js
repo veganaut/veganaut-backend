881f9eb4ecefe606562a6f14a9d90642ca80a0eb
Tobias Leugger <leugger.tobias@web.de>
2014-09-28 19:01:15 +0200

    Returning empty object by default if geo ip location couldn't be found.

--- app/controllers/GeoIP.js
+++ app/controllers/GeoIP.js
@@ -1,25 +1,20 @@
 'use strict';
 
+var geoip = require('geoip-lite');
+
 /**
  * Returns the current location of user based on her IP
  * @param req
  * @param res
- * @param next
  */
 exports.get = function(req, res) {
-    var geoip = require('geoip-lite');
-
     var ip = req.connection.remoteAddress;
-    var geo = geoip.lookup(ip);
+    var geo = geoip.lookup(ip) || {};
     
     // Add separate fields for latitude and longitude
-    if (geo !== null && typeof geo.ll === 'object') {
+    if (typeof geo.ll === 'object') {
         geo.lat = geo.ll[0];
         geo.lng = geo.ll[1];
-    }
-    else {
-        // default location is Bern
-        geo = { 'lat': 46.95, 'lng': 7.45 };
     }
 
     return res.send(geo);
