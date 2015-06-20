bba646c01150e8b8ef84e5776472e99950af163b
Tobias Leugger <leugger.tobias@web.de>
2015-06-20 20:59:52 +0200

    Adding migration to add availability info to products.

---
+++ migrations/2015-06-20-add-availability-to-product.js
@@ -0,0 +1,22 @@
+/* global db */
+/**
+ * Migration script to add availability=100 (the default value)
+ * to all existing products
+ */
+db.products.find().forEach(function(product) {
+    'use strict';
+
+    try {
+        db.products.update(
+            {_id: product._id},
+            {
+                $set: {
+                    availability: 100 // 100 is the value for "available"
+                }
+            }
+        );
+    }
+    catch (e) {
+        print(e);
+    }
+});
