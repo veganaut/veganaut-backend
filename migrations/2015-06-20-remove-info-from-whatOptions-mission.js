655d0794badb5559ddae91c26bc45bac572fadbd
Tobias Leugger <leugger.tobias@web.de>
2015-06-20 21:10:33 +0200

    Adding migration to remove "info" from all whatOptions mission outcomes.

---
+++ migrations/2015-06-20-remove-info-from-whatOptions-mission.js
@@ -0,0 +1,28 @@
+/* global db */
+/**
+ * Migrations script to remove the "info" field from all whatOptions mission
+ * outcomes.
+ */
+db.missions.find({__t: 'WhatOptionsMission'}).forEach(function(mission) {
+    'use strict';
+
+    // Remove info from all outcomes
+    var newOutcome = mission.outcome;
+    newOutcome.forEach(function(productInfo) {
+        delete productInfo.info;
+    });
+
+    try {
+        db.missions.update(
+            {_id: mission._id},
+            {
+                $set: {
+                    outcome: newOutcome
+                }
+            }
+        );
+    }
+    catch (e) {
+        print(e);
+    }
+});
