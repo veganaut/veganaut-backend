d246b07e9093e2fd16e79a69b999d8b8a6e588e4
Tobias Leugger <leugger.tobias@web.de>
2015-08-25 17:12:55 +0200

    Writing the giant migration that gets rid of teams and makes the owning of locations person-based.

--- migrations/WIP-remove-teams.js
+++
@@ -1,9 +0,0 @@
-/**
- * TODO
- */
-
-// TODO: remove team property from person and location
-// TODO: add owner property to location
-// TODO: update location points to be a map of user id to points
-// TODO: add accountType to person (team = npc -> accountType = npc)
-// TODO: convert mission points from object to simple number
